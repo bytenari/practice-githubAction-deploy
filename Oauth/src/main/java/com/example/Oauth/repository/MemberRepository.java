5afe4b7c67a57148afbc63f43bb7685c367e0744
bytenari <bytenari@gmail.com>
2022-08-08 14:24:27 +0900

    3rd

--- Oauth/src/main/java/com/example/Oauth/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Oauth.repository;
-
-import com.example.Oauth.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    public Member findByUsername(String username);
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# practice-githubAction-deploy
